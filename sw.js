50e57b5702da000b54085853fc337b009c292f4f
Amitoje Singh Bakshi <amitojeSBakshi@users.noreply.github.com>
2026-08-18 23:09:52 +0530

    Refresh Bangalore Circle board

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-﻿/* Bangalore Circle - build 20260817-163839-8214 */
-var CACHE = "blr-circle-20260817-163839-8214";
+﻿/* Bangalore Circle - build 20260818-173653-6670 */
+var CACHE = "blr-circle-20260818-173653-6670";
 var ASSETS = ["./", "./index.html", "./manifest.json",
               "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png"];
 
